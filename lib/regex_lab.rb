62c7d61282b19aacb2361652df96959606e0634d
clifford <clifford@noukod.com>
2018-09-14 14:16:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,10 +21,11 @@ return false
   else
     return true
 end
-
+end
 def valid_phone_number?(phone)
 if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
   return false
     else
       return true
+end
 end
